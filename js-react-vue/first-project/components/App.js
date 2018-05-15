c509a604ca7da27743ebd194c36fa13a5e7c45b8
Julian Phillips <julian.p@me.com>
2018-05-15 12:31:40 -0400

    Completed App.js

--- js-react-vue/first-project/components/App.js
+++ js-react-vue/first-project/components/App.js
@@ -36,7 +36,7 @@ var App = React.createclass({
 
     // - Switch if a location is a favorite.
 
-    toggleFavorites(address){
+    toggleFavorite(address){
 
         if(this.isAddressInFavorites(address)){
             this.removeFromFavorites(address);
@@ -81,6 +81,99 @@ var App = React.createclass({
         var favorites = this.state.favorites;
 
         var index = -1;
-        
+
+        // - Loop through the favorites array.
+
+        for(var i = 0; i < favorites.length; i++){
+
+            if(favorites[i].address == address){
+                index = i;
+                break;
+            }
+        }
+
+        // - If it is found, remove it from the favorites array
+
+        if(index !== -1){
+
+            favorites.splice(index, 1);
+
+            this.setState({
+                favorites: favorites
+            });
+
+            localStorage.favorites = JSON.stringify(favorites);
+        }
+    },
+
+    // - This will determine if an address is in favorites.
+
+    isAddressInFavorites(address){
+
+        // - Get current state of favorites
+
+        var favorites = this.state.favorites;
+
+        for(var i = 0; i < favorites.length; i++){
+
+            if(favorites.address == address){
+                return true;
+            }
+
+        }
+
+        return false;
+    },
+
+    // - Search for an address
+
+    searchForAddress(address){
+
+        var self = this;
+
+        // - Using GMaps geocode functionality, which is built on the Google Maps API.
+
+        GMaps.geocode({
+            address: address,
+            callback: function(result, status){
+
+                if(status !== 'OK') return;
+
+                var latlng = results[0].geometry.location;
+
+                self.setState({
+                    currentAddress: results[0].formatted_address,
+                    mapCoordinates: {
+                        lat: latlng.lat(),
+                        lng: latlng.lng()
+                    }
+                });
+            }
+        });
+    },
+
+    // - Rendering the App.js file
+
+    render(){
+
+        return (
+                <div>
+                <h1>Your Google Maps Locations</h1>
+
+                <Search onSearch={this.searchForAddress} />
+
+                <Map lat={this.state.mapCoordinates.lat} lng={this.state.mapCoordinates.lng} />
+
+                <CurrentLocation address={this.state.currentAddress}
+            favorite={this.state.isAddressInFavorites(this.state.currentAddress)}
+            onFavoriteToggle={this.toggleFavorite} />
+
+                <LocationList locations={this.state.favorites} activeLocationAddress={this.state.currentAddress} onClick={this.searchForAddress} />
+
+            </div>
+
+        );
     }
-})
+});
+
+module.exports = App;
