8fbc0317ccd93b84abda9d5163ac5e55db954ef8
Julian Phillips <julian.p@me.com>
2018-05-16 13:15:13 -0400

    Starting the second project in Vue

---
+++ js-react-vue/second-project/app.js
@@ -0,0 +1,15 @@
+// app.js
+
+new Vue({
+    // Target the div with the id of events.
+    el: '#events',
+
+    // Data collector
+    data: {},
+
+    // Functions that will run when the application loads.
+    ready: function(){},
+
+    // Methods for use within the application.
+    methods: {}
+});
